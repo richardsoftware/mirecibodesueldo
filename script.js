dda8d3c8010d58360450cc3f7b94c64e4e2d1c93
Ricardo <138032193+richardsoftware@users.noreply.github.com>
2025-09-04 09:51:05 -0300

    AGOSTO NUEVO

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function calcularSueldo() {
     let redondeo1 = totalRedondo - totalAPagar;
 
     document.getElementById('result').innerHTML = `
-        <h2>Detalle del Sueldo     Mes: Agosto 2025 Nueva Paritaria</h2>
+        <h2>Detalle del Sueldo  Mes: Agosto 2025 Nueva Paritaria</h2>
         <p>Nombre: ${nombre}</p>
         <table>
             <tr><td>1000</td><td>Sueldo</td><td>1</td><td>$ ${sueldo.toFixed(2)}</td></tr>
